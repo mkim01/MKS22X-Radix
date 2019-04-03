853e5a60b3d3a344cbb69a28190093768c541f80
Minuk Kim <mkim01@stuy.edu>
2019-04-03 16:44:11 -0400

    getMax method to biggest digit

---
+++ Radix.java
@@ -0,0 +1,27 @@
+public static void radixsort(int[] data){
+  //MyLinkedList<Integer> bucket = new MyLinkedList();
+}
+
+public static int getMax(int[] data){
+
+  int maxdigit = 0;
+  for (int i = 0; i < data.length){
+    if (data[i] > maxDigit){
+      maxdigit = digit;
+    }
+  }
+  return maxdigit;
+}
+
+  // MyLinkedList<
+  // for(int i = 0; i <= 9; i++){
+  //   bucket.add(i);
+  // }
+  //
+  // for(int i = 0; i < data.length; i++){
+  //   int remain = data[i] % 10;
+  //   if (remain >)
+  //   bucket.add(data){
+  //   }
+  // }
+}
