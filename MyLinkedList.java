e23b4b79e50443d955262547cf032c5b50d87d0f
Minuk Kim <mkim01@stuy.edu>
2019-04-04 01:26:52 -0400

    fix extend and removefonrt

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -34,6 +34,8 @@ public class MyLinkedList<E>{
     private Node start,end;
 
     public MyLinkedList(){
+      start = null;
+      end = start;
       length = 0;
     }
 
@@ -73,24 +75,40 @@ public class MyLinkedList<E>{
       return true;
     }
 
+    public int size(){
+      return length;
+    }
+
+
     public void extend (MyLinkedList other){
-      if (other.length!= 0){
-        this.end.setNext(other.start);
-        other.start.setPrev(this.end);
-        this.end = other.end;
+        end.setNext(other.start);
+        length += other.size();
+        end = other.end;
         other.start = null;
         other.end = null;
-        length += other.length;
         other.length = 0;
     }
-  }
+
+    private Node getNthNode(int N){
+      Node current = start;
+      for (int i = 0; i < N; i++) {
+        current = current.next();
+      }
+      return current;
+    }
 
     public E removeFront(){
-      E val = getNth
-      start = start.next();
-      start.setPrev(null);
+      E val = getNthNode(0).getData();
+      if (length == 1){
+        start = null;
+        end = start;
+      }
+      else{
+        start = getNthNode(1);
+        start.prev().setPrev(null);
+      }
       length--;
-      return start.getData();
+      return val;
     }
 
     // public static void radixsort(int[] data){
